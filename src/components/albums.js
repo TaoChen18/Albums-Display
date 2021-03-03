00b5354f01ebd53624cbbbaf6c25e46935ad7f6f
TaoChen18 <waldochan18@gmail.com>
2021-03-03 02:41:19 -0500

    Layout and basic responsive design added

--- src/components/albums.js
+++ src/components/albums.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { List, Card, Row, Col } from 'antd';
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
-
+//import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import './Albums.css';
+import Searchbar from './Searchbar';
 
 const Albums = () => {
     const [data, setData] = useState(null)
@@ -23,20 +24,37 @@ const Albums = () => {
         let count = 0;
         const tmp = [];
         while(count < 4){
-            const current = data?.feed.entry[i];
             const pic = data?.feed.entry[i]["im:image"][2].label;
+            const name = data?.feed.entry[i]["im:name"].label;
+            const album_link = data?.feed.entry[i].link.attributes.href;
+            const right = data?.feed.entry[i].rights.label;
+            const artist = data?.feed.entry[i]["im:artist"].label;
+            const artist_link = data?.feed.entry[i]["im:artist"].attributes?.href;
+            const price = data?.feed.entry[i]["im:price"].label;
+            const num = data?.feed.entry[i]["im:itemCount"].label;
+            const category = data?.feed.entry[i].category.attributes.label;
+            const release_date = data?.feed.entry[i]["im:releaseDate"].attributes.label;
             tmp.push({
-                href: current?.link.attributes.href,
-                title: current?.title.label,
                 picture: pic,
+                name: name,
+                album_href: album_link,
+                artist: artist,
+                artist_href: artist_link,
+                price: price,
+                num: num,
+                category: category,
+                release: release_date,
+                company: right
+                
             });
             i++;
             count++;
         }
-        console.log(tmp)
         listData.push(tmp);
     }
     return(
+        <div className="cards">
+            
         <List itemLayout="vertical" size="small" pagination={{
             onChange: page => {
         console.log(page);
@@ -47,60 +65,89 @@ const Albums = () => {
     renderItem={item => (
         <Row gutter={24}>
             <Col span={6}>
-                <Card
-                style={{width:275}}
-                cover={<img alt="example" src={item[0].picture}/>}
-                actions={[
-                    <SettingOutlined key="setting" />,
-                    <EditOutlined key="edit" />,
-                    <EllipsisOutlined key="ellipsis" />,
-                ]}
+                <a className='card_link' href={item[0].album_href}><Card
+                className='card_item'
+                cover={
+                        <img className='card_picture' 
+                        alt="example" src={item[0].picture}/>}
+                // actions={[
+                //     <SettingOutlined key="setting" />,
+                //     <EditOutlined key="edit" />,
+                //     <EllipsisOutlined key="ellipsis" />,
+                // ]}
                 >
-                    {item[0].title}
-                </Card>
+                    <div className='card_info'>
+                        <h5 className='card_text'>{item[0].name}</h5>
+                        <h5 className='artist'>{item[0].artist}</h5>
+                        <h5 className='release_date'>{item[0].release}</h5>
+                        <h5 className='price'>{item[0].price}</h5>
+                    </div>
+                </Card></a>
             </Col>
             <Col span={6}>
-                <Card
-                style={{width:275}}
-                cover={<img alt="example" src={item[1].picture}/>}
-                actions={[
-                    <SettingOutlined key="setting" />,
-                    <EditOutlined key="edit" />,
-                    <EllipsisOutlined key="ellipsis" />,
-                ]}
+                <a className='card_link' href={item[1].album_href}><Card
+                className='card_item'
+                cover={
+                        <img className='card_picture' 
+                        alt="example" src={item[1].picture}/>}
+                // actions={[
+                //     <SettingOutlined key="setting" />,
+                //     <EditOutlined key="edit" />,
+                //     <EllipsisOutlined key="ellipsis" />,
+                // ]}
                 >
-                    {item[1].title}
-                </Card>
+                    <div className='card_info'>
+                        <h5 className='card_text'>{item[1].name}</h5>
+                        <h5 className='artist'>{item[1].artist}</h5>
+                        <h5 className='release_date'>{item[1].release}</h5>
+                        <h5 className='price'>{item[1].price}</h5>
+                    </div>
+                </Card></a>
             </Col>
             <Col span={6}>
-                <Card
-                style={{width:275}}
-                cover={<img alt="example" src={item[2].picture}/>}
-                actions={[
-                    <SettingOutlined key="setting" />,
-                    <EditOutlined key="edit" />,
-                    <EllipsisOutlined key="ellipsis" />,
-                ]}
+                <a className='card_link' href={item[2].album_href}><Card
+                className='card_item'
+                cover={
+                        <img className='card_picture' 
+                        alt="example" src={item[2].picture}/>}
+                // actions={[
+                //     <SettingOutlined key="setting" />,
+                //     <EditOutlined key="edit" />,
+                //     <EllipsisOutlined key="ellipsis" />,
+                // ]}
                 >
-                    {item[2].title}
-                </Card>
+                    <div className='card_info'>
+                        <h5 className='card_text'>{item[2].name}</h5>
+                        <h5 className='artist'>{item[2].artist}</h5>
+                        <h5 className='release_date'>{item[2].release}</h5>
+                        <h5 className='price'>{item[2].price}</h5>
+                    </div>
+                </Card></a>
             </Col>
             <Col span={6}>
-                <Card
-                style={{width:275}}
-                cover={<img alt="example" src={item[3].picture}/>}
-                actions={[
-                    <SettingOutlined key="setting" />,
-                    <EditOutlined key="edit" />,
-                    <EllipsisOutlined key="ellipsis" />,
-                ]}
+                <a className='card_link' href={item[3].album_href}><Card
+                className='card_item'
+                cover={
+                        <img className='card_picture' 
+                        alt="example" src={item[3].picture}/>}
+                // actions={[
+                //     <SettingOutlined key="setting" />,
+                //     <EditOutlined key="edit" />,
+                //     <EllipsisOutlined key="ellipsis" />,
+                // ]}
                 >
-                    {item[3].title}
-                </Card>
+                    <div className='card_info'>
+                        <h5 className='card_text'>{item[3].name}</h5>
+                        <h5 className='artist'>{item[3].artist}</h5>
+                        <h5 className='release_date'>{item[3].release}</h5>
+                        <h5 className='price'>{item[3].price}</h5>
+                    </div>
+                </Card></a>
             </Col>
         </Row>
     )}
   />
+  </div>
     )
 }
 
