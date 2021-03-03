c3e9d66db30ef2e375f9b2c8e3db22ea7151da63
TaoChen18 <waldochan18@gmail.com>
2021-03-03 05:47:26 -0500

    search functionality added

--- src/components/Albumlist.js
+++ src/components/Albumlist.js
@@ -3,6 +3,30 @@ import './Albums.css';
 import { List, Card, Row, Col } from 'antd';
 
 const Albumlist = ({albumlist = []}) => {
+    const renderoption = (content) => {
+        if(content){
+            return <Col span={6}>
+            <a className='card_link' href={content?.album_href}><Card
+            className='card_item'
+            cover={
+                    <img className='card_picture' 
+                    alt="example" src={content?.picture}/>}
+            // actions={[
+            //     <SettingOutlined key="setting" />,
+            //     <EditOutlined key="edit" />,
+            //     <EllipsisOutlined key="ellipsis" />,
+            // ]}
+            >
+                <div className='card_info'>
+                    <h5 className='card_text'>{content?.name}</h5>
+                    <h5 className='artist'>{content?.artist}</h5>
+                    <h5 className='release_date'>{content?.release}</h5>
+                    <h5 className='price'>{content?.price}</h5>
+                </div>
+            </Card></a>
+        </Col>
+        }
+    }
     return (
         <div className="cards">
         <List itemLayout="vertical" size="small" pagination={{
@@ -34,66 +58,10 @@ const Albumlist = ({albumlist = []}) => {
                     </div>
                 </Card></a>
             </Col>
-            <Col span={6}>
-                <a className='card_link' href={item[1].album_href}><Card
-                className='card_item'
-                cover={
-                        <img className='card_picture' 
-                        alt="example" src={item[1].picture}/>}
-                // actions={[
-                //     <SettingOutlined key="setting" />,
-                //     <EditOutlined key="edit" />,
-                //     <EllipsisOutlined key="ellipsis" />,
-                // ]}
-                >
-                    <div className='card_info'>
-                        <h5 className='card_text'>{item[1].name}</h5>
-                        <h5 className='artist'>{item[1].artist}</h5>
-                        <h5 className='release_date'>{item[1].release}</h5>
-                        <h5 className='price'>{item[1].price}</h5>
-                    </div>
-                </Card></a>
-            </Col>
-            <Col span={6}>
-                <a className='card_link' href={item[2].album_href}><Card
-                className='card_item'
-                cover={
-                        <img className='card_picture' 
-                        alt="example" src={item[2].picture}/>}
-                // actions={[
-                //     <SettingOutlined key="setting" />,
-                //     <EditOutlined key="edit" />,
-                //     <EllipsisOutlined key="ellipsis" />,
-                // ]}
-                >
-                    <div className='card_info'>
-                        <h5 className='card_text'>{item[2].name}</h5>
-                        <h5 className='artist'>{item[2].artist}</h5>
-                        <h5 className='release_date'>{item[2].release}</h5>
-                        <h5 className='price'>{item[2].price}</h5>
-                    </div>
-                </Card></a>
-            </Col>
-            <Col span={6}>
-                <a className='card_link' href={item[3].album_href}><Card
-                className='card_item'
-                cover={
-                        <img className='card_picture' 
-                        alt="example" src={item[3].picture}/>}
-                // actions={[
-                //     <SettingOutlined key="setting" />,
-                //     <EditOutlined key="edit" />,
-                //     <EllipsisOutlined key="ellipsis" />,
-                // ]}
-                >
-                    <div className='card_info'>
-                        <h5 className='card_text'>{item[3].name}</h5>
-                        <h5 className='artist'>{item[3].artist}</h5>
-                        <h5 className='release_date'>{item[3].release}</h5>
-                        <h5 className='price'>{item[3].price}</h5>
-                    </div>
-                </Card></a>
-            </Col>
+            
+            {renderoption(item[1])}
+            {renderoption(item[2])}
+            {renderoption(item[3])}
         </Row>
     )}
   />
--- src/components/albums.js
+++ src/components/albums.js
@@ -2,38 +2,59 @@ import React, { useEffect, useState } from 'react';
 import { List, Card, Row, Col } from 'antd';
 //import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import './Albums.css';
-import Searchbar from './Searchbar';
+import Albumlist from './Albumlist';
+import { Input, Space } from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
+
+const { Search } = Input;
 
 const Albums = () => {
     const [data, setData] = useState(null)
+    const [dataDefault, setDataDefault] = useState(null);
+    const [input, setInput] = useState(null);
+
     const fetchURL = "https://itunes.apple.com/us/rss/topalbums/limit=100/json"
     
     useEffect(() => {
         getData();
     }, []);
-    
+
     const getData = async () => {
         const response = await fetch(fetchURL);
         const jsonData = await response.json();
-        setData(jsonData)
+        setData(jsonData.feed.entry);
+        setDataDefault(jsonData.feed.entry)
+    }
+
+    const onSearch = async (input) => {
+        const filtered = []
+        for(let i = 0; i < dataDefault.length;i++){
+            if(dataDefault[i]["im:name"].label.toLowerCase().includes(input.toLowerCase())||
+            dataDefault[i]["im:artist"].label.toLowerCase().includes(input.toLowerCase())){
+                filtered.push(dataDefault[i]);
+            }
+        }
+        console.log(filtered)
+        setInput(input);
+        setData(filtered);
     }
 
     const listData = [];
     let i = 0;
-    while(i < 100){
+    while(i < data?.length){
         let count = 0;
         const tmp = [];
-        while(count < 4){
-            const pic = data?.feed.entry[i]["im:image"][2].label;
-            const name = data?.feed.entry[i]["im:name"].label;
-            const album_link = data?.feed.entry[i].link.attributes.href;
-            const right = data?.feed.entry[i].rights.label;
-            const artist = data?.feed.entry[i]["im:artist"].label;
-            const artist_link = data?.feed.entry[i]["im:artist"].attributes?.href;
-            const price = data?.feed.entry[i]["im:price"].label;
-            const num = data?.feed.entry[i]["im:itemCount"].label;
-            const category = data?.feed.entry[i].category.attributes.label;
-            const release_date = data?.feed.entry[i]["im:releaseDate"].attributes.label;
+        while(count < 4 && i < data?.length){
+            const pic = data[i]["im:image"][2].label;
+            const name = data[i]["im:name"].label;
+            const album_link = data[i].link.attributes.href;
+            const right = data[i].rights.label;
+            const artist = data[i]["im:artist"].label;
+            const artist_link = data[i]["im:artist"].attributes?.href;
+            const price = data[i]["im:price"].label;
+            const num = data[i]["im:itemCount"].label;
+            const category = data[i].category.attributes.label;
+            const release_date = data[i]["im:releaseDate"].attributes.label;
             tmp.push({
                 picture: pic,
                 name: name,
@@ -52,102 +73,24 @@ const Albums = () => {
         }
         listData.push(tmp);
     }
+
     return(
-        <div className="cards">
-            
-        <List itemLayout="vertical" size="small" pagination={{
-            onChange: page => {
-        console.log(page);
-      },
-      pageSize: 5,
-    }}
-    dataSource={listData}
-    renderItem={item => (
-        <Row gutter={24}>
-            <Col span={6}>
-                <a className='card_link' href={item[0].album_href}><Card
-                className='card_item'
-                cover={
-                        <img className='card_picture' 
-                        alt="example" src={item[0].picture}/>}
-                // actions={[
-                //     <SettingOutlined key="setting" />,
-                //     <EditOutlined key="edit" />,
-                //     <EllipsisOutlined key="ellipsis" />,
-                // ]}
-                >
-                    <div className='card_info'>
-                        <h5 className='card_text'>{item[0].name}</h5>
-                        <h5 className='artist'>{item[0].artist}</h5>
-                        <h5 className='release_date'>{item[0].release}</h5>
-                        <h5 className='price'>{item[0].price}</h5>
-                    </div>
-                </Card></a>
-            </Col>
-            <Col span={6}>
-                <a className='card_link' href={item[1].album_href}><Card
-                className='card_item'
-                cover={
-                        <img className='card_picture' 
-                        alt="example" src={item[1].picture}/>}
-                // actions={[
-                //     <SettingOutlined key="setting" />,
-                //     <EditOutlined key="edit" />,
-                //     <EllipsisOutlined key="ellipsis" />,
-                // ]}
-                >
-                    <div className='card_info'>
-                        <h5 className='card_text'>{item[1].name}</h5>
-                        <h5 className='artist'>{item[1].artist}</h5>
-                        <h5 className='release_date'>{item[1].release}</h5>
-                        <h5 className='price'>{item[1].price}</h5>
-                    </div>
-                </Card></a>
-            </Col>
-            <Col span={6}>
-                <a className='card_link' href={item[2].album_href}><Card
-                className='card_item'
-                cover={
-                        <img className='card_picture' 
-                        alt="example" src={item[2].picture}/>}
-                // actions={[
-                //     <SettingOutlined key="setting" />,
-                //     <EditOutlined key="edit" />,
-                //     <EllipsisOutlined key="ellipsis" />,
-                // ]}
-                >
-                    <div className='card_info'>
-                        <h5 className='card_text'>{item[2].name}</h5>
-                        <h5 className='artist'>{item[2].artist}</h5>
-                        <h5 className='release_date'>{item[2].release}</h5>
-                        <h5 className='price'>{item[2].price}</h5>
-                    </div>
-                </Card></a>
-            </Col>
-            <Col span={6}>
-                <a className='card_link' href={item[3].album_href}><Card
-                className='card_item'
-                cover={
-                        <img className='card_picture' 
-                        alt="example" src={item[3].picture}/>}
-                // actions={[
-                //     <SettingOutlined key="setting" />,
-                //     <EditOutlined key="edit" />,
-                //     <EllipsisOutlined key="ellipsis" />,
-                // ]}
-                >
-                    <div className='card_info'>
-                        <h5 className='card_text'>{item[3].name}</h5>
-                        <h5 className='artist'>{item[3].artist}</h5>
-                        <h5 className='release_date'>{item[3].release}</h5>
-                        <h5 className='price'>{item[3].price}</h5>
-                    </div>
-                </Card></a>
-            </Col>
-        </Row>
-    )}
-  />
-  </div>
+        <>
+        <div className="search_bar">
+        <Space direction="vertical">
+            <Search
+            placeholder="Enter Album Name or Artist Name..."
+            allowClear
+            enterButton="Search"
+            size="large"
+            onSearch={onSearch}
+            />
+        </Space>
+        </div>
+        
+        <Albumlist albumlist={listData} />
+        
+        </>
     )
 }
 
