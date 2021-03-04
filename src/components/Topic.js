e1f00c3d7b4381cc15d38118b748e1550ddc7409
TaoChen18 <waldochan18@gmail.com>
2021-03-04 06:01:06 -0500

    animation added

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -6,11 +6,11 @@ function Topic() {
   return (
     <div className='topic-container'>
       <img alt='background' src='https://vidooly.com/blog/wp-content/uploads/2019/09/Top-10-Websites-to-Find-Background-Music-for-your-Videos-featured-Image.jpg'
-      style={{'object-fit': 'cover',
+      style={{'objectFit': 'cover',
         'width': '100%',
         'height': '100%',
         'position': 'fixed',
-        'z-index': '-1'}}/>
+        'zIndex': '-1'}}/>
       <h1>TOP 100 ALBUMS</h1>
       <p>Which one is your pick?</p>
     </div>
--- src/components/albums.js
+++ src/components/albums.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { List, Card, Row, Col } from 'antd';
-//import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { List, Card, Input, Space,Tag, Collapse } from 'antd';
+import { AppleOutlined } from '@ant-design/icons';
 import './Albums.css';
-import Albumlist from './Albumlist';
-import { Input, Space } from 'antd';
-import { AudioOutlined } from '@ant-design/icons';
+
 
 const { Search } = Input;
 
@@ -12,12 +10,22 @@ const Albums = () => {
     const [data, setData] = useState(null)
     const [dataDefault, setDataDefault] = useState(null);
     const [input, setInput] = useState(null);
+    const [play, setPlay] = useState(true);
+    const [duration, setDuration] = useState(0);
+    const [index, setIndex] = useState({});
 
     const fetchURL = "https://itunes.apple.com/us/rss/topalbums/limit=100/json"
     
     useEffect(() => {
         getData();
     }, []);
+
+    useEffect(() =>{
+        if(duration===0){
+            setPlay(true);
+            setDuration(1);
+        }
+    },[duration]);
 
     const getData = async () => {
         const response = await fetch(fetchURL);
@@ -34,44 +42,44 @@ const Albums = () => {
                 filtered.push(dataDefault[i]);
             }
         }
-        console.log(filtered)
         setInput(input);
         setData(filtered);
+        setPlay(false);
+        setDuration(0);
     }
-
     const listData = [];
     let i = 0;
     while(i < data?.length){
-        let count = 0;
-        const tmp = [];
-        while(count < 4 && i < data?.length){
-            const pic = data[i]["im:image"][2].label;
-            const name = data[i]["im:name"].label;
-            const album_link = data[i].link.attributes.href;
-            const right = data[i].rights.label;
-            const artist = data[i]["im:artist"].label;
-            const artist_link = data[i]["im:artist"].attributes?.href;
-            const price = data[i]["im:price"].label;
-            const num = data[i]["im:itemCount"].label;
-            const category = data[i].category.attributes.label;
-            const release_date = data[i]["im:releaseDate"].attributes.label;
-            tmp.push({
-                picture: pic,
-                name: name,
-                album_href: album_link,
-                artist: artist,
-                artist_href: artist_link,
-                price: price,
-                num: num,
-                category: category,
-                release: release_date,
-                company: right
-                
-            });
-            i++;
-            count++;
-        }
-        listData.push(tmp);
+        const pic = data[i]["im:image"][2].label;
+        const name = data[i]["im:name"].label;
+        const album_link = data[i].link.attributes.href;
+        const right = data[i].rights.label;
+        const artist = data[i]["im:artist"].label;
+        const artist_link = data[i]["im:artist"].attributes?.href;
+        const price = data[i]["im:price"].label;
+        const num = data[i]["im:itemCount"].label;
+        const category = data[i].category.attributes.label;
+        const release_date = data[i]["im:releaseDate"].attributes.label;
+        listData.push({
+            id: i,
+            picture: pic,
+            name: name,
+            album_href: album_link,
+            artist: artist,
+            artist_href: artist_link,
+            price: price,
+            num: num,
+            category: category,
+            release: release_date,
+            company: right
+            
+        });
+        i++;
+    }
+
+    const expandCard = (i) => {
+        console.log(i)
+        setIndex({...setIndex,[i]: !index[i]})
     }
 
     return(
@@ -88,7 +96,62 @@ const Albums = () => {
         </Space>
         </div>
         
-        <Albumlist albumlist={listData} />
+        <div className="cards">
+            
+        <List itemLayout="vertical" size="small" pagination={{
+            onChange: page => {
+        setPlay(false);
+        setDuration(0);
+      },
+      pageSize: 20,
+    }}
+    dataSource={listData}
+    renderItem={item => (
+                <div className="card_container"
+                style={{
+                    'width' : `${index[item.id] ? 550 : 250}px`,
+                    'height' : `${index[item.id] ? 450 : 450}px`,
+                    'transition' : 'width 1s',
+                    'animation': `${play ? 'fade-in':""} 1s`,
+                    'animationDuration': `${0.2*duration}s`
+                }}
+                >
+                <Card
+                className='card_link stack_top'style={{
+                    'animation': `${play ? 'fade-in':""} 1s`,
+                    'animationDuration': `${0.2*duration}s`
+                }}
+                onClick={() => {expandCard(item.id)}}
+                cover={
+                        <img className='card_picture' 
+                        alt="example" src={item.picture}/>}
+                >
+                    <div className='card_info'>
+                        <h5 className='card_text'>{item.name}</h5>
+                        <h5 className='artist'>{item.artist}</h5>
+                        <h5 className='release_date'>{item.release}</h5>
+                        <h5 className='price'>{item.price}</h5>
+                        <Tag className='category' color="blue">{item.category}</Tag>
+                    </div>
+                </Card>
+                <div className='card_detail'
+                style={{
+                    'left' : `${index[item.id] ? 280 : 30}px`,
+                    'transition' : 'left 1s',
+                }}
+                >
+                        <h5 className='album'><span>{item.name}</span></h5>
+                        <div className='album_link'><a href={item.album_href} target='_blank' style={{'color':'black'}}>Learn more<AppleOutlined className="apple_link"/></a></div>
+                        <h5 className='artist_in'><span>Artist: </span><a href={item.artist_href} target="_blank">{item.artist}</a></h5>
+                        <h5 className='release_date_in'><span>Release Date: </span>{item.release}</h5>
+                        <h5 className='company'><span>Company: </span>{item.company}</h5>
+                        <h5 className='item_count'>{item.num}</h5>
+                    </div>
+                </div>
+                
+    )}
+  />
+  </div>
         
         </>
     )
